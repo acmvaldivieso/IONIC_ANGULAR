d12ef09c3ca19444792c6fbb56f0a1f64f317aeb
Sebacaffi <sebastian.caffi@gmail.com>
2021-10-19 15:22:01 -0300

    seteo logo en componente

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { IonicModule } from '@ionic/angular';
+import { LogoComponent } from './logo/logo.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    LogoComponent
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    LogoComponent
   ],
   imports: [
     CommonModule,
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
+import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    ComponentsModule
   ],
   declarations: [LoginPage]
 })
